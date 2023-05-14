d264d9a74938ff3ddf3dcec261634a678bfa42f2
AdamZWinter <adam@bootstrapmusic.com>
2023-05-14 16:53:12 +0000

    do not neet 2775 on other directories

--- usersetup.sh
+++ usersetup.sh
@@ -11,6 +11,6 @@ fi
 
 sudo mkdir -p /var/www/$1/public_html
 sudo chown -R $1:root /var/www/$1
-sudo chmod -R 2775 /var/www/$1
+sudo chmod -R 2775 /var/www/$1/public_html
 sudo ln -s /var/www/$1/public_html /home/$1/public_html
 
