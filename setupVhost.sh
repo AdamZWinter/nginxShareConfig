5dbb55df2fa1fb0b9acfc1f46f8cf5403d3c4988
AdamZWinter <adam@bootstrapmusic.com>
2023-05-14 03:02:34 +0000

    now setup to use public_html, not project

--- setupVhost.sh
+++ setupVhost.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-echo requires four parameters: host.domain.tld port username project
+echo requires four parameters: host.domain.tld port username
 echo $1
 echo $2
 echo $3
-echo $4
 
-if [ -z "$4" ]
+if [ -z "$3" ]
 then
 	echo wrong number of arguments
 	exit
@@ -17,7 +16,7 @@ sudo cp host.domain.tld /etc/nginx/vhosts/
 sudo sed -i 's/{{host.domain.tld}}/'$1'/g' /etc/nginx/vhosts/host.domain.tld
 sudo sed -i 's/{{port}}/'$2'/g' /etc/nginx/vhosts/host.domain.tld
 sudo sed -i 's/{{username}}/'$3'/g' /etc/nginx/vhosts/host.domain.tld
-sudo sed -i 's/{{project}}/'$4'/g' /etc/nginx/vhosts/host.domain.tld
+#sudo sed -i 's/{{project}}/'$4'/g' /etc/nginx/vhosts/host.domain.tld
 
 sudo mv /etc/nginx/vhosts/host.domain.tld /etc/nginx/vhosts/$1
 sudo chmod 644 /etc/nginx/vhosts/$1
--- usersetup.sh
+++ usersetup.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-echo requires two arguments: username project
+echo requires one arguments: username
 echo $1
-echo $2
 
-if [ -z "$2" ]
+if [ -z "$1" ]
 then
-	echo wrong number of arguments
+	echo error no username provided
 	        exit
 fi
 
-sudo mkdir -p /var/www/$1/$2
+sudo mkdir -p /var/www/$1/public_html
 sudo chown -R $1:root /var/www/$1
 sudo chmod -R 2775 /var/www/$1
-sudo cp -r /usr/share/nginx/html /var/www/$1/$2
-sudo chown -R $1:root /var/www/$1
-sudo ln -s /var/www/$1/$2 /home/$1/$2
+sudo ln -s /var/www/$1/public_html /home/$1/public_html
 
