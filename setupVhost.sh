00321460157c4f4ff6a62cab89841969ed588d41
AdamZWinter <adam@bootstrapmusic.com>
2023-05-15 19:21:18 +0000

    updated for reactport

--- setupVhost.sh
+++ setupVhost.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-echo requires four parameters: host.domain.tld port username
+echo requires four parameters: host.domain.tld expressport username reactport
 echo $1
 echo $2
 echo $3
+echo $4
 
-if [ -z "$3" ]
+if [ -z "$4" ]
 then
 	echo wrong number of arguments
 	exit
@@ -16,7 +17,7 @@ sudo cp host.domain.tld /etc/nginx/vhosts/
 sudo sed -i 's/{{host.domain.tld}}/'$1'/g' /etc/nginx/vhosts/host.domain.tld
 sudo sed -i 's/{{port}}/'$2'/g' /etc/nginx/vhosts/host.domain.tld
 sudo sed -i 's/{{username}}/'$3'/g' /etc/nginx/vhosts/host.domain.tld
-#sudo sed -i 's/{{project}}/'$4'/g' /etc/nginx/vhosts/host.domain.tld
+sudo sed -i 's/{{reactport}}/'$4'/g' /etc/nginx/vhosts/host.domain.tld
 
 sudo mv /etc/nginx/vhosts/host.domain.tld /etc/nginx/vhosts/$1
 sudo chmod 644 /etc/nginx/vhosts/$1
